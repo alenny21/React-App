c01a0b216ef982c7bd49af3c3d0f8c801648b4d9
Angelo Troncone <angelotroncone@gmail.com>
2023-05-31 11:30:29 -0600

    Use state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import ListGroup from "./ListGroup"
+import ListGroup from "./ListGroup";
 
-function App(){
-  return <div><ListGroup/></div>
+function App() {
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/ListGroup.js
+++ src/ListGroup.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  let selectedIndex = -1;
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const getMessage = () => {
     return items.length === 0 && <p>No item found</p>;
@@ -16,7 +18,15 @@ function ListGroup() {
       {getMessage()}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className={"list-group-item"} key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
